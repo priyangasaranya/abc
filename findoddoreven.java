9cbfc8449526bab36913cba20db812528d652052
priyangasaranya <34539230+priyangasaranya@users.noreply.github.com>
2019-02-13 12:25:17 +0530

    Create findoddoreven.java

---
+++ findoddoreven.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class findoddoreven
+{
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+    if(n%2==0)
+      {
+       System.out.print("even");
+       }
+     else
+     {
+       System.out.print("odd");
+     }
+ }
+}
